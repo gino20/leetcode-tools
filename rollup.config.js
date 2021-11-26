257b5cdf5113002a63ddc604856cef6b33bd0a56
gino20 <gino20.hong@gmail.com>
2021-11-27 00:54:35 +0800

    feat: add link to leetcode-cn in leetcode

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import metablock from "rollup-plugin-userscript-metablock";
+import typescriptPlugin from "@rollup/plugin-typescript";
+import typescript from "typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+const pkg = require("./package.json");
+
+export default {
+  input: "src/user.ts",
+  output: {
+    file: "dist/user.js",
+    format: "iife",
+    sourcemap: true,
+  },
+  plugins: [
+    serve("dist"),
+    livereload(),
+    commonjs({
+      include: ["node_modules/**"],
+      exclude: ["node_modules/process-es6/**"],
+    }),
+    nodeResolve({ extensions: [".js", ".ts", ".tsx"] }),
+    typescriptPlugin({ typescript }),
+    metablock({
+      file: "./meta.json",
+      override: {
+        name: pkg.name,
+        version: pkg.version,
+        description: pkg.description,
+        homepage: pkg.homepage,
+        author: pkg.author,
+        license: pkg.license,
+      },
+    }),
+  ],
+};
