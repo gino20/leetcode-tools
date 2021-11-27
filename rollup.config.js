e0b0d7c906fb658522fb812f8ece899b7afd1163
gino20 <gino20.hong@gmail.com>
2021-11-27 16:39:35 +0800

    fix: fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import metablock from "rollup-plugin-userscript-metablock";
-import typescriptPlugin from "@rollup/plugin-typescript";
-import typescript from "typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
+import metablock from 'rollup-plugin-userscript-metablock';
+import typescriptPlugin from '@rollup/plugin-typescript';
+import typescript from 'typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 export default {
-  input: "src/user.ts",
+  input: 'src/user.ts',
   output: {
-    file: "dist/user.js",
-    format: "iife",
+    file: 'dist/user.js',
+    format: 'iife',
     sourcemap: true,
   },
   plugins: [
-    serve("dist"),
-    livereload(),
+    // serve("dist"),
+    // livereload(),
     commonjs({
-      include: ["node_modules/**"],
-      exclude: ["node_modules/process-es6/**"],
+      include: ['node_modules/**'],
+      exclude: ['node_modules/process-es6/**'],
     }),
-    nodeResolve({ extensions: [".js", ".ts", ".tsx"] }),
+    nodeResolve({ extensions: ['.js', '.ts', '.tsx'] }),
     typescriptPlugin({ typescript }),
     metablock({
-      file: "./meta.json",
+      file: './meta.json',
       override: {
         name: pkg.name,
         version: pkg.version,
